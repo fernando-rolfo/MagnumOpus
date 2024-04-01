0e7da2f4085bc1ade5b368588fab7dd08925d096
fernando-rolfo <fernandorolfo@gmail.com>
2024-04-01 19:20:18 -0300

    + adjustments

---
+++ index.js
@@ -0,0 +1,11 @@
+// function showCourseText(index) {
+//     // Obtener el elemento del párrafo
+//     var courseTexts = document.getElementsByClassName("course-text");
+
+//     // Cambiar la visibilidad del párrafo
+//     if (courseTexts[index].hidden) {
+//         courseTexts[index].removeAttribute("hidden");
+//     } else {
+//         courseTexts[index].setAttribute("hidden", true);
+//     }
+// }
